d5ae2591ee545c50ad2a7a7bea3c92650cdb0345
Akshay Kumar S B <jinmori58690@gmail.com>
2023-03-26 11:09:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = os.environ.get("API_ID", "21927988")
 
 API_HASH = os.environ.get("API_HASH", "e18f720acdff1e5b0ec80616aecd8a5a")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6180096542:AAF0bV80V-Rl2mNOojrq4i1ail58ekneJc8") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "WalhallaBots") 
 
@@ -16,7 +16,7 @@ DB_URL = os.environ.get("DB_URL","mongodb+srv://dokjakim155:TBQApLQFWj4QUrzV@tel
  
 FLOOD = int(os.environ.get("FLOOD", "100"))
 
-START_PIC = os.environ.get("START_PIC", "")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/53362e855bfb0501190d0.jpg")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
